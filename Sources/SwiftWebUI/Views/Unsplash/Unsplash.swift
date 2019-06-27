de32f7ee8a8988f349ccc376144b29b09a498034
Helge Heß <me@helgehess.eu>
2019-06-27 14:39:01 +0200

    Add Unsplash stuff
    Easily created Unsplash images.

---
+++ Sources/SwiftWebUI/Views/Unsplash/Unsplash.swift
@@ -0,0 +1,27 @@
+//
+//  Unsplash.swift
+//  SwiftWebUI
+//
+//  Created by Helge Heß on 25.06.19.
+//  Copyright © 2019 Helge Heß. All rights reserved.
+//
+
+extension Image {
+  
+  public init(_ source: UnsplashSource, label: Text? = nil) {
+    self.storage = .url(source.url, label: label)
+  }
+  
+  public static func unsplash(scope : UnsplashSource.Scope = .none,
+                              time  : UnsplashSource.Time  = .all,
+                              size  : UXSize = UXSize(width: 640, height: 480),
+                              _ terms : String...) -> Image
+  {
+    return Image(UnsplashSource(
+      scope: scope, time: time,
+      size:
+      UnsplashSource.Size(width: Int(size.width), height: Int(size.height)),
+      terms: terms)
+    )
+  }
+}
---
+++ Sources/SwiftWebUI/Views/Unsplash/UnsplashSource.swift
@@ -0,0 +1,73 @@
+//
+//  UnsplashSource.swift
+//  SwiftWebUI
+//
+//  Created by Helge Heß on 25.06.19.
+//  Copyright © 2019 Helge Heß. All rights reserved.
+//
+
+import struct Foundation.URL
+import struct Foundation.URLComponents
+
+// https://source.unsplash.com
+public struct UnsplashSource {
+  
+  public enum Scope {
+    case none
+    case featured
+    case user(String)
+    case collection(String)
+    
+    var pathComponents: [ String ] {
+      switch self {
+        case .none:              return []
+        case .featured:          return [ "featured"      ]
+        case .user(let user):    return [ "user", user    ]
+        case .collection(let v): return [ "collection", v ]
+      }
+    }
+  }
+  
+  public enum Time {
+    case all, daily, weekly
+    
+    var pathComponent: String {
+      switch self {
+        case .all:    return ""
+        case .daily:  return "daily"
+        case .weekly: return "weekly"
+      }
+    }
+  }
+  
+  public struct Size {
+    public var width  : Int = 640
+    public var height : Int = 480
+    
+    var pathComponent: String { return "\(width)x\(height)" }
+  }
+  
+  var scope : Scope
+  var time  : Time
+  var size  : Size?
+  var terms : [ String ]
+  
+  var url: URL {
+    var pathComponents = scope.pathComponents + [ time.pathComponent ]
+    if let size = size { pathComponents.append(size.pathComponent) }
+    
+    var url = URLComponents()
+    url.scheme = "https"
+    url.host   = "source.unsplash.com"
+    url.path   = "/" + pathComponents.map {
+      $0.addingPercentEncoding(withAllowedCharacters: .urlPathAllowed) ?? $0
+    }.joined(separator: "/")
+    
+    url.query = terms.map {
+      $0.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) ?? $0
+    }.joined(separator: ",")
+
+    return url.url!
+  }
+}
+
