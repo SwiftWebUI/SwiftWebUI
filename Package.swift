4f89346ddc2fa1038d2dea9228fd2fdcca75d93e
Helge Heß <me@helgehess.eu>
2019-07-04 15:01:54 +0200

    Add HelloWorld test app
    ... just to test whether things run at all.

--- Package.swift
+++ Package.swift
@@ -3,29 +3,30 @@
 import PackageDescription
 
 let package = Package(
-    name: "SwiftWebUI",
-    platforms: [
-       .macOS(.v10_15), .iOS(.v13)
-    ],
-    products: [
-        .library(name: "SwiftWebUI", targets: [ "SwiftWebUI" ])
-    ],
-    dependencies: [
-        .package(url: "https://github.com/apple/swift-nio.git",
-                 from: "2.3.0"),
-        .package(url: "https://github.com/SwiftWebResources/SemanticUI-Swift.git",
-                 from: "2.3.3"),
-        .package(url: "https://github.com/wickwirew/Runtime.git",
-                 from: "2.1.0"),
-        .package(url: "https://github.com/onmyway133/SwiftHash.git",
-                 from: "2.0.2")
-                 
-    ],
-    targets: [
-        .target(name: "SwiftWebUI",
-                dependencies: [ 
-                    "NIO", "NIOHTTP1", "NIOConcurrencyHelpers", 
-                    "SwiftHash", "Runtime", "SemanticUI" 
-                ])
-    ]
+  name: "SwiftWebUI",
+  platforms: [
+    .macOS(.v10_15), .iOS(.v13)
+  ],
+  products: [
+    .library   (name: "SwiftWebUI", targets: [ "SwiftWebUI" ]),
+    .executable(name: "HolyCow",    targets: [ "HolyCow"    ])
+  ],
+  dependencies: [
+    .package(url: "https://github.com/apple/swift-nio.git",
+             from: "2.3.0"),
+    .package(url: "https://github.com/SwiftWebResources/SemanticUI-Swift.git",
+             from: "2.3.4"),
+    .package(url: "https://github.com/wickwirew/Runtime.git",
+             from: "2.1.0"),
+    .package(url: "https://github.com/onmyway133/SwiftHash.git",
+             from: "2.0.2")               
+  ],
+  targets: [
+    .target(name: "SwiftWebUI",
+            dependencies: [ 
+                "NIO", "NIOHTTP1", "NIOConcurrencyHelpers", 
+                "SwiftHash", "Runtime", "SemanticUI" 
+            ]),
+    .target(name: "HolyCow", dependencies: [ "SwiftWebUI" ])
+  ]
 )
